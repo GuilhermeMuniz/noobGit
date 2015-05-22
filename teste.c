beb785b8f5f866639c74afd5c747a96ad325ea04
Guilherme Muniz <guilherme.cmuniz@gmail.com>
2015-05-22 11:40:12 -0300

    segundo teste com repositorio

---
+++ teste.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main () {
+
+int x =0, y=0 , z=0,i;
+
+	for(i =0; i < 10; i++){
+		if(i%2==0)
+		x++;
+		if(i%3==0)
+		y++;
+		if(i%5==0)
+		z++;
+
+		}
+
+	printf("/n x= %d y=%d z =%d /n");
+
+
+system("PAUSE");
+
+}
+
